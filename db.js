db7a1d8bf3684b087f7242fb80891e0a2a9226b8
Gautierrr <gaut.vauloup@gmail.com>
2024-02-21 16:15:16 +0100

    Update with database connection

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // User
+  process.env.DB_PASSWORD, // Password
+  // ou directement le lien 'internale' ou 'externale'
   {
-    host: '', // Host
+    host: process.env.DB_HOST, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
